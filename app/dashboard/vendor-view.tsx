bbb45e89d0444283e27528cb592dc9e868cf2124
Priyatosh Kadam <85998054+PriyatoshKadam@users.noreply.github.com>
2026-09-11 18:29:19 +0530

    fix: mount event issue timestamp enhancer

--- app/dashboard/vendor-view.tsx
+++ app/dashboard/vendor-view.tsx
@@ -2,7 +2,8 @@
 
 import VendorView from './vendor-view-pdf';
 import ReferenceMonitoringPanels from './reference-monitoring-panels';
+import EventIssueTimestampEnhancer from './event-issue-timestamp-enhancer';
 
 export default function VendorViewWithReferenceMonitoring(props: { vendor: string; label: string; id: string | null }) {
-  return <><VendorView {...props} /><ReferenceMonitoringPanels vendor={props.vendor} /></>;
+  return <><VendorView {...props} /><ReferenceMonitoringPanels vendor={props.vendor} /><EventIssueTimestampEnhancer vendor={props.vendor} /></>;
 }
