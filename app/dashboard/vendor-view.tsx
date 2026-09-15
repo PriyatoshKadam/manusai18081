23dc3e966fe36686ee832d72586c4eebc83070da
Priyatosh Kadam <85998054+PriyatoshKadam@users.noreply.github.com>
2026-09-15 12:46:48 +0530

    feat: mount platform governance coverage

--- app/dashboard/vendor-view.tsx
+++ app/dashboard/vendor-view.tsx
@@ -6,7 +6,8 @@ import EventIssueTimestampEnhancer from './event-issue-timestamp-enhancer';
 import ParameterGovernanceTableEnhancer from './parameter-governance-table-enhancer';
 import PageGovernanceTableEnhancer from './page-governance-table-enhancer';
 import ConsentGovernanceEnhancer from './consent-governance-enhancer';
+import PlatformGovernanceEnhancer from './platform-governance-enhancer';
 
 export default function VendorViewWithReferenceMonitoring(props: { vendor: string; label: string; id: string | null }) {
-  return <><VendorView {...props} /><ReferenceMonitoringPanels vendor={props.vendor} /><EventIssueTimestampEnhancer vendor={props.vendor} /><ParameterGovernanceTableEnhancer vendor={props.vendor} /><PageGovernanceTableEnhancer vendor={props.vendor} /><ConsentGovernanceEnhancer vendor={props.vendor} /></>;
+  return <><VendorView {...props} /><ReferenceMonitoringPanels vendor={props.vendor} /><PlatformGovernanceEnhancer vendor={props.vendor} /><EventIssueTimestampEnhancer vendor={props.vendor} /><ParameterGovernanceTableEnhancer vendor={props.vendor} /><PageGovernanceTableEnhancer vendor={props.vendor} /><ConsentGovernanceEnhancer vendor={props.vendor} /></>;
 }
