68cfad42a18108ef4d1540fd31ece09ef5e257af
Priyatosh Kadam <85998054+PriyatoshKadam@users.noreply.github.com>
2026-09-15 12:18:34 +0530

    feat: mount consent governance

--- app/dashboard/vendor-view.tsx
+++ app/dashboard/vendor-view.tsx
@@ -5,7 +5,8 @@ import ReferenceMonitoringPanels from './reference-monitoring-panels';
 import EventIssueTimestampEnhancer from './event-issue-timestamp-enhancer';
 import ParameterGovernanceTableEnhancer from './parameter-governance-table-enhancer';
 import PageGovernanceTableEnhancer from './page-governance-table-enhancer';
+import ConsentGovernanceEnhancer from './consent-governance-enhancer';
 
 export default function VendorViewWithReferenceMonitoring(props: { vendor: string; label: string; id: string | null }) {
-  return <><VendorView {...props} /><ReferenceMonitoringPanels vendor={props.vendor} /><EventIssueTimestampEnhancer vendor={props.vendor} /><ParameterGovernanceTableEnhancer vendor={props.vendor} /><PageGovernanceTableEnhancer vendor={props.vendor} /></>;
+  return <><VendorView {...props} /><ReferenceMonitoringPanels vendor={props.vendor} /><EventIssueTimestampEnhancer vendor={props.vendor} /><ParameterGovernanceTableEnhancer vendor={props.vendor} /><PageGovernanceTableEnhancer vendor={props.vendor} /><ConsentGovernanceEnhancer vendor={props.vendor} /></>;
 }
