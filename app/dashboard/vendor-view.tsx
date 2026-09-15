e19b135879116e110a9a1662618c10f71532e5b5
Priyatosh Kadam <85998054+PriyatoshKadam@users.noreply.github.com>
2026-09-15 12:04:53 +0530

    feat: mount Trackingplan-style pages governance

--- app/dashboard/vendor-view.tsx
+++ app/dashboard/vendor-view.tsx
@@ -4,7 +4,8 @@ import VendorView from './vendor-view-pdf';
 import ReferenceMonitoringPanels from './reference-monitoring-panels';
 import EventIssueTimestampEnhancer from './event-issue-timestamp-enhancer';
 import ParameterGovernanceTableEnhancer from './parameter-governance-table-enhancer';
+import PageGovernanceTableEnhancer from './page-governance-table-enhancer';
 
 export default function VendorViewWithReferenceMonitoring(props: { vendor: string; label: string; id: string | null }) {
-  return <><VendorView {...props} /><ReferenceMonitoringPanels vendor={props.vendor} /><EventIssueTimestampEnhancer vendor={props.vendor} /><ParameterGovernanceTableEnhancer vendor={props.vendor} /></>;
+  return <><VendorView {...props} /><ReferenceMonitoringPanels vendor={props.vendor} /><EventIssueTimestampEnhancer vendor={props.vendor} /><ParameterGovernanceTableEnhancer vendor={props.vendor} /><PageGovernanceTableEnhancer vendor={props.vendor} /></>;
 }
